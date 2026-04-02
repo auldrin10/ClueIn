d7b6147bd23c8c7e3f8a4f4c4b3107dcbe666ff0
Auldrin Matlala <auldrinproject@gmail.com>
2026-04-02 14:56:24 +0200

    Renamed signin to signup

--- app/src/main/java/com/example/cluein/SignUpActivity.java
+++ app/src/main/java/com/example/cluein/SignUpActivity.java
@@ -1,6 +1,5 @@
 package com.example.cluein;
 
-import android.app.appsearch.GetByDocumentIdRequest;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Editable;
@@ -15,15 +14,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import java.io.ObjectInputValidation;
-
-public class SigninActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_signin);
+        setContentView(R.layout.activity_signup);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
