ac7d936043a310b1ad9ebbf007c8d38c47f4ad79
Brighton <manganyibrighton2@gmail.com>
2026-04-04 23:27:58 +0200

    Reset Password Page is added

--- app/src/main/java/com/example/cluein/LostPasswordActivity.java
+++ app/src/main/java/com/example/cluein/LostPasswordActivity.java
@@ -10,38 +10,23 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class LoginActivity extends AppCompatActivity {
+public class LostPasswordActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_lost_password);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
-    public void ToDashboard(View v){
 
-        Intent Dashboard = new Intent(this, MainActivity.class);
-        startActivity(Dashboard);
+    public void ToLogIn(View v){
+
+        Intent LogIn = new Intent(this, LoginActivity.class);
+        startActivity(LogIn);
     }
-    public void ToSignUp(View v){
-
-        Intent SignUp = new Intent(this, SignUpActivity.class);
-        startActivity(SignUp);
-    }
-
-    public void ResetActivity(View v){
-
-        Intent ResetPage = new Intent(this, LostPasswordActivity.class);
-        startActivity(ResetPage);
-    }
-
-
-
-
-
 }
--- app/src/main/java/com/example/cluein/SignUpActivity.java
+++ app/src/main/java/com/example/cluein/SignUpActivity.java
@@ -1,5 +1,6 @@
 package com.example.cluein;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Editable;
@@ -162,6 +163,11 @@ public class SignUpActivity extends AppCompatActivity {
             }
         });
     }
+    //This function is called when the user click Log In text from sign up activity
+    public void ToLogIn(View v){
 
+        Intent LogIn = new Intent(this, LoginActivity.class);
+        startActivity(LogIn);
+    }
 
 }
