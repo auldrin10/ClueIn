d7fc394e9ffb44125e6086d96ae115bc01204417
Auldrin Matlala <auldrinproject@gmail.com>
2026-04-16 16:24:46 +0200

    Update ProfileFragment

---
+++ app/src/main/java/com/example/cluein/MainFragment.java
@@ -0,0 +1,19 @@
+package com.example.cluein;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+public class MainFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        return view;
+    }
+}
