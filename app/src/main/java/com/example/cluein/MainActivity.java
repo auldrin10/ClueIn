ff510acdddb8291fe6f125d9b856198e4ccf8982
Brighton <manganyibrighton2@gmail.com>
2026-04-16 17:09:03 +0200

    Updated to fragments

--- app/src/main/java/com/example/cluein/MainActivity.java
+++ app/src/main/java/com/example/cluein/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.cluein;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
