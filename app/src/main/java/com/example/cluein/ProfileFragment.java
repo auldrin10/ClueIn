e981a228edfb8b331466fa9a1c4ebcbccdc7ee2d
Auldrin Matlala <auldrinproject@gmail.com>
2026-04-16 17:08:53 +0200

    Update ProfileFragment

--- app/src/main/java/com/example/cluein/ProfileFragment.java
+++ app/src/main/java/com/example/cluein/ProfileFragment.java
@@ -2,20 +2,55 @@ package com.example.cluein;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import java.util.*;
 
 public class ProfileFragment extends Fragment {
+    private TextView username;
+    private TextView tvProfileUserName;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
 
-        return view;
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        tvProfileUserName = view.findViewById(R.id.profileUserName);
+        
+        // Use a String variable to store the text from the TextView/EditText
+        String name = "";
+        if (tvProfileUserName != null) {
+            name = tvProfileUserName.getText().toString();
+        }
+        
+        String[] splitted = name.split(" ");
+        String initials = "";
+
+        if (splitted.length > 0) {
+            String firstName = splitted[0];
+            String lastName = splitted[splitted.length - 1];
+
+            if (!firstName.isEmpty()) {
+                initials += firstName.charAt(0);
+            }
+            if (splitted.length > 1 && !lastName.isEmpty()) {
+                initials += lastName.charAt(0);
+            }
+        }
+
+        String upperInitials = initials.toUpperCase();
+        tvProfileUserName.setText(upperInitials);
     }
 }
