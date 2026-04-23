50781ff193ded54f8890237e76094b3c02267ecc
Auldrin Matlala <auldrinproject@gmail.com>
2026-04-23 23:09:12 +0200

    Create more fragments in profile fragment

---
+++ app/src/main/java/com/example/cluein/LogOut.java
@@ -0,0 +1,20 @@
+package com.example.cluein;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class LogOut extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v  = inflater.inflate(R.layout.fragment_log_out, container, false);
+        return v;
+    }
+}
---
+++ app/src/main/java/com/example/cluein/UpdateProfilePic.java
@@ -0,0 +1,22 @@
+package com.example.cluein;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class UpdateProfilePic extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v =  inflater.inflate(R.layout.fragment_update_profile_pic, container, false);
+        return v;
+    }
+}
