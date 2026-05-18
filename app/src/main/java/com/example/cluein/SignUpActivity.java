8df53ece1f48a1e68675b489bcf4fdfb5ecac3e9
Auldrin Matlala <auldrinproject@gmail.com>
2026-05-18 19:57:13 +0200

    Update social icons

--- app/src/main/java/com/example/cluein/SignUpActivity.java
+++ app/src/main/java/com/example/cluein/SignUpActivity.java
@@ -50,14 +50,6 @@ import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
-import com.facebook.AccessToken;
-import com.facebook.CallbackManager;
-import com.facebook.FacebookCallback;
-import com.facebook.FacebookException;
-import com.facebook.FacebookSdk;
-import com.facebook.login.LoginManager;
-import com.facebook.login.LoginResult;
-import com.google.firebase.auth.FacebookAuthProvider;
 import java.util.Arrays;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
@@ -72,7 +64,6 @@ public class SignUpActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private GoogleSignInClient mGoogleSignInClient;
     private ActivityResultLauncher<Intent> googleSignInLauncher;
-    private CallbackManager mCallbackManager;
 
 
     @Override
@@ -80,10 +71,6 @@ public class SignUpActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         
-        // Explicitly initialize Facebook SDK before any LoginManager calls
-        FacebookSdk.setApplicationId(getString(R.string.facebook_app_id));
-        FacebookSdk.sdkInitialize(getApplicationContext());
-
         setContentView(R.layout.activity_signup);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -115,8 +102,6 @@ public class SignUpActivity extends AppCompatActivity {
         View dot1 = findViewById(R.id.dot1);
 
         ImageView btnGoogle = findViewById(R.id.btnGoogle);
-        ImageView btnInstagram = findViewById(R.id.btnInstagram);
-        ImageView btnFacebook = findViewById(R.id.btnFacebook);
 
         // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
@@ -149,32 +134,7 @@ public class SignUpActivity extends AppCompatActivity {
                 }
         );
 
-        // Initialize Facebook Login
-        mCallbackManager = CallbackManager.Factory.create();
-        LoginManager.getInstance().registerCallback(mCallbackManager,
-                new FacebookCallback<LoginResult>() {
-                    @Override
-                    public void onSuccess(LoginResult loginResult) {
-                        handleFacebookAccessToken(loginResult.getAccessToken());
-                    }
-
-                    @Override
-                    public void onCancel() {
-                        Log.d("FB_AUTH", "facebook:onCancel");
-                    }
-
-                    @Override
-                    public void onError(FacebookException error) {
-                        Log.d("FB_AUTH", "facebook:onError", error);
-                        Toast.makeText(SignUpActivity.this, "Facebook Login Failed", Toast.LENGTH_SHORT).show();
-                    }
-                });
-
         btnGoogle.setOnClickListener(v -> signInWithGoogle());
-        btnInstagram.setOnClickListener(v -> startInstagramAuth());
-        btnFacebook.setOnClickListener(v -> {
-            LoginManager.getInstance().logInWithReadPermissions(this, mCallbackManager, Arrays.asList("email", "public_profile"));
-        });
 
         /* Password only appears when users focuses on the inputbox */
         password.setOnFocusChangeListener((v, hasFocus) -> {
@@ -431,130 +391,9 @@ public class SignUpActivity extends AppCompatActivity {
                 });
     }
 
-    private void handleFacebookAccessToken(AccessToken token) {
-        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
-        mAuth.signInWithCredential(credential)
-                .addOnCompleteListener(this, task -> {
-                    if (task.isSuccessful()) {
-                        FirebaseUser firebaseUser = mAuth.getCurrentUser();
-                        if (firebaseUser != null) {
-                            getSharedPreferences("UserPrefs", Context.MODE_PRIVATE)
-                                    .edit()
-                                    .putString("USER_EMAIL", firebaseUser.getEmail())
-                                    .apply();
-
-                            Toast.makeText(this, "Facebook Registration Successful", Toast.LENGTH_SHORT).show();
-                            
-                            Intent intent = new Intent(this, MainActivity.class);
-                            intent.putExtra("USER_EMAIL", firebaseUser.getEmail());
-                            startActivity(intent);
-                            finish();
-                        }
-                    } else {
-                        Toast.makeText(SignUpActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
-                    }
-                });
-    }
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        mCallbackManager.onActivityResult(requestCode, resultCode, data);
-    }
-
-    private void startInstagramAuth() {
-        String clientId = getString(R.string.instagram_client_id);
-        String redirectUri = getString(R.string.instagram_redirect_uri);
-        String authUrl = "https://api.instagram.com/oauth/authorize" +
-                "?client_id=" + clientId +
-                "&redirect_uri=" + redirectUri +
-                "&scope=user_profile,user_media" +
-                "&response_type=code";
-
-        InstagramDialog dialog = new InstagramDialog(this, authUrl, redirectUri, code -> {
-            getInstagramToken(code);
-        });
-        dialog.show();
-    }
-
-    private void getInstagramToken(String code) {
-        String clientId = getString(R.string.instagram_client_id);
-        String clientSecret = getString(R.string.instagram_client_secret);
-        String redirectUri = getString(R.string.instagram_redirect_uri);
-
-        RequestBody formBody = new FormBody.Builder()
-                .add("client_id", clientId)
-                .add("client_secret", clientSecret)
-                .add("grant_type", "authorization_code")
-                .add("redirect_uri", redirectUri)
-                .add("code", code)
-                .build();
-
-        Request request = new Request.Builder()
-                .url("https://api.instagram.com/oauth/access_token")
-                .post(formBody)
-                .build();
-
-        userclient.newCall(request).enqueue(new Callback() {
-            @Override
-            public void onFailure(@NonNull Call call, @NonNull IOException e) {
-                runOnUiThread(() -> Toast.makeText(SignUpActivity.this, "Instagram Token Error", Toast.LENGTH_SHORT).show());
-            }
-
-            @Override
-            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
-                if (response.isSuccessful()) {
-                    try {
-                        JSONObject json = new JSONObject(response.body().string());
-                        String accessToken = json.getString("access_token");
-                        String userId = json.getString("user_id");
-                        getInstagramUserProfile(accessToken, userId);
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        });
-    }
-
-    private void getInstagramUserProfile(String accessToken, String userId) {
-        String url = "https://graph.instagram.com/" + userId + "?fields=id,username&access_token=" + accessToken;
-
-        Request request = new Request.Builder().url(url).build();
-        userclient.newCall(request).enqueue(new Callback() {
-            @Override
-            public void onFailure(@NonNull Call call, @NonNull IOException e) {
-                runOnUiThread(() -> Toast.makeText(SignUpActivity.this, "Instagram Profile Error", Toast.LENGTH_SHORT).show());
-            }
-
-            @Override
-            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
-                if (response.isSuccessful()) {
-                    try {
-                        JSONObject json = new JSONObject(response.body().string());
-                        String username = json.getString("username");
-
-                        runOnUiThread(() -> {
-                            String placeholderEmail = username + "@instagram.com";
-                            
-                            getSharedPreferences("UserPrefs", Context.MODE_PRIVATE)
-                                    .edit()
-                                    .putString("USER_EMAIL", placeholderEmail)
-                                    .apply();
-
-                            Toast.makeText(SignUpActivity.this, "Instagram Registration: " + username, Toast.LENGTH_SHORT).show();
-                            
-                            Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
-                            intent.putExtra("USER_EMAIL", placeholderEmail);
-                            startActivity(intent);
-                            finish();
-                        });
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        });
     }
 
     private void openUrl(String url) {
